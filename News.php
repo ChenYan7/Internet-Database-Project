f692f88782cace389533c585c9507a1158d5d27c
zhl-00 <64481314+zhl-00@users.noreply.github.com>
2020-06-08 22:50:12 +0800

    Add files via upload

---
+++ News.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Team:
+coding by ZhengHuilin 1811522
+*/
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%dxynews}}".
+ *
+ * @property string|null $pubDate
+ * @property string|null $title
+ * @property string|null $summary
+ * @property string|null $infoSource
+ * @property string|null $sourceTrl
+ * @property string|null $province
+ * @property string|null $provinceId
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%dxynews}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['pubDate', 'title', 'summary', 'infoSource', 'sourceTrl', 'province', 'provinceId'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'pubDate' => 'Pub Date',
+            'title' => 'Title',
+            'summary' => 'Summary',
+            'infoSource' => 'Info Source',
+            'sourceTrl' => 'Source Trl',
+            'province' => 'Province',
+            'provinceId' => 'Province ID',
+        ];
+    }
+}
